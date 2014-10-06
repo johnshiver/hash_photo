4d7be20abcac21e1d56241b8cdd0b5cc203c7a2f
John Shiver <johneshiver@gmail.com>
2014-10-06 10:33:41 -0700

    Updated hash function

--- hash_photo/photos/tasks.py
+++ hash_photo/photos/tasks.py
@@ -1,16 +1,17 @@
 from celery import task
 from .models import ItemPhoto
-
-
-@task
-def add(x, y):
-    return x + y
+import pHash
+from django.core.files.temp import NamedTemporaryFile
 
 
 @task
 def dummy_hash(itemPhoto_id):
     recent_photo = ItemPhoto.objects.get(pk=itemPhoto_id)
-    recent_photo.phash = 'thisIsABigHashNumber'
+    # recent_photo.phash = 'thisIsABigHashNumber'
+    temp = NamedTemporaryFile(delete=False)
+    temp.write(recent_photo.photo.read())
+    hash1 = pHash.imagehash(temp.name)
+    recent_photo.phash = str(hash1)
     is_duplicate = ItemPhoto.objects.filter(phash=recent_photo.phash).exists()
     recent_photo.duplicate = is_duplicate
     recent_photo.save(elevation=False)
